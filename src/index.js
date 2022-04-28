32944fad4d3f83dd8f98eadf22441a0c20f3b741
Alan <john1337@gmail.com>
2022-04-28 18:28:32 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
+let port = process.env.PORT || 3000;
 /* import "./models/Project.js";
 import "./models/Task.js"; */
 //Conexion a la base de datos
 async function main() {
   try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
     await sequelize.sync({ force: false });
-    app.listen(3000);
-
+    app.listen(port, () => {
+      console.log(`Puerto ${port}`);
+    });
     console.log("Server activo");
   } catch (error) {
     console.log("NO se pudo conectar a la db");
